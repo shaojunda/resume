30c9c0fa21cbe335fbc6c6d9bf4c23d45aea63ef
shaojunda <shaojunda@gmail.com>
2018-07-20 15:32:00 +0800

    ADD: nav bar style

---
+++ summery.mk
@@ -0,0 +1,43 @@
+## css 布局
+
+1. 横向
+
+2. 纵向
+
+
+## a 标签
+
+1. target
+2. download
+3. href
+	<a href="//qq.com"></a> 使用当前的协议访问 qq.com
+	`<a href="#sdfs"></a> 锚点`
+	<a href="?name=xxx"></a> 向当前地方发送一次请求（附带参数）
+	<a href="javascript: alert(1);">QQ</a> 伪协议
+	<a href="javascript:;">QQ</a> 点击之后不跳转
+
+# iframe
+
+frameborder=0
+
+name 属性和 a 配合使用
+
+
+## 安装 http server
+`npm i -g http-server`
+
+`http-server -c-1` 不缓存
+
+## form
+target 属性
+
+# label
+包裹 input 可以省去 id
+
+# css
+@import
+
+
+# 横向布局
+1. 所有子元素添加 float: left
+2. 父元素添加 clearfix
