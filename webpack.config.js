d7be8454e83ef40bf240d716c77e68018945c4d7
shaojunda <shaojunda@gmail.com>
2019-02-20 21:26:41 +0800

    CHG: use url loader

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,17 @@ module.exports = {
           }
         }]
       },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
     ]
   },
   plugins: [
